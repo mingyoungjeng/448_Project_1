eeab23d7e98e58e7ed5566a3b0b0e5666dc422b0
Mingyoung Jeng <mingyoungjeng@gmail.com>
2021-02-18 21:44:23 -0600

    Added templates for new JS classes

--- Board.js
+++ Board.js
@@ -3,12 +3,7 @@ class Board {
 		this.cells = []; // this may be useful later when iterating.
 		this.table = document.createElement("table");
 		this.table.class = "game_board";
-
-		if (id) {
-			this.table.id = id;
-		} else {
-			throw "error: ID is required for Board class!";
-		}
+		this.table.id = id ? id : "Board";
 
 		let rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 		let cols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
@@ -19,7 +14,8 @@ class Board {
 			this.cells.push([]);
 			for (var j = 0; j < cols.length; j++) {
 				var cell = row.insertCell(j);
-				cell.id = id + "_" + cols[j] + rows[i];
+				cell.class = cols[j] + rows[i]; // generic name
+				cell.id = this.table.id + "_" + cols[j] + rows[i]; // unique name
 
 				this.cells[i].push(cell);
 			}
@@ -28,6 +24,14 @@ class Board {
 	}
 
 	getCell(id) {
-		return document.getElementById(this.table.id + "_" + id);
+		for (var i = 0; i < this.cells.length; i++) {
+			for (var j = 0; j < this.cells[i].length; j++) {
+				if (this.cells[i][j].class == id) {
+					return this.cells[i][j];
+				}
+			}
+		}
+		console.log("Board " + this.table.id + " does not have cell " + id + "!");
+		return null;
 	}
 }
---
+++ Game.js
@@ -0,0 +1,32 @@
+// Game.js
+// Controls the logic of the game.
+
+class Game {
+
+	// Sets up a new game
+	setup() {
+		// Creates two game boards on screen
+
+		// The following must be done for Player 1 AND Player 2
+		// Select how many ships they want to place
+		// Place ships
+
+		// Start game
+	}
+
+	// Runs a game while playing
+	play() {
+		// Alternates turns between players.
+
+		// Each turn:
+		// Player selects square to shoot
+		// Detects hit or miss
+		// Update history
+		// Prompt player to end turn
+	}
+
+	// Triggered when one player wins
+	game_over() {
+
+	}
+}
