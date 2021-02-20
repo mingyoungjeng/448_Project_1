b88932bedf404923bf553eef2094428f4c54fe15
johnportin <jportin13@gmail.com>
2021-02-19 18:34:40 -0600

    Add place() to Ship.js. Part of ship placement

--- Ship.js
+++ Ship.js
@@ -1,12 +1,14 @@
 class Ship {
 	constructor(locations = []) {
-		this.locations = {}; // An object that stores locations with values true/false. True = not hit
+		this.locations = locations; // An object that stores locations with values true/false. True = not hit
 		this.size = locations.length;
 		this.health = this.size; //decrement on hit
 
 		for (var cell of locations) {
 			this.locations[cell] = true;
 		}
+		this.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		this.cols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 	}
 
 	isSunk() {
@@ -24,6 +26,24 @@ class Ship {
 
 	draw() {
 
+	}
+
+	place(row, col, num, orientation) {
+		if (orientation === "vertical") {
+			for (var i = 0; i < num; i++) {
+				var temp = this.cols[col + i] + this.rows[row];
+				this.locations[i] = `${this.cols[col + i]}${this.rows[row]}`;
+				console.log(temp);
+				document.querySelector(`td#player1_${this.cols[col + i]}${this.rows[row]}`).setAttribute('hasShip', true);
+			}
+		}
+		else if (orientation === "horizontal") {
+			for (var i = 0; i < num; i++) {
+				this.locations[i] = `${this.cols[col]}${this.rows[row + i]}`;
+				document.querySelector(`td#player1_${this.cols[col]}${this.rows[row + i]}`).setAttribute('hasShip', true);
+			}
+		}
+		
 	}
 
 }
